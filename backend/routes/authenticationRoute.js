cfbf334f226b416f146ad14ccb7f936761937a7b
SanidhyaMadheshia <sanidhyamadheshia@gmail.com>
2024-09-16 15:18:38 +0530

    whole project with basic frontend

---
+++ backend/routes/authenticationRoute.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+
+
+
+const router = express.Router();
+
+
+
+const {authController}=require('../controllers/authController')
+
+
+
+
+
+
+router.post('/authentication',authController)
+
+module.exports=router
