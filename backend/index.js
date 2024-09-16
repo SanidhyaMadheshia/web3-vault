299746c4e119397d94b0e769dc2064e9e8bf1fb4
SanidhyaMadheshia <139427620+SanidhyaMadheshia@users.noreply.github.com>
2024-09-16 19:22:47 +0530

    alllowed all orign

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,43 @@
 const express = require('express');
-const app = express()
-const cors = require('cors')
-const {MONGODB_URL,PORT}=require('./config/serverConfig')
-const {connectDB}=require('./db/connect')
-const authenticationRoute=require('./routes/authenticationRoute')
-const uploadImageRoute=require('./routes/uploadImageRoute')
-const getImageRoute=require('./routes/getImageRoute')
+const cors = require('cors');
+const { MONGODB_URL, PORT } = require('./config/serverConfig');
+const { connectDB } = require('./db/connect');
+const authenticationRoute = require('./routes/authenticationRoute');
+const uploadImageRoute = require('./routes/uploadImageRoute');
+const getImageRoute = require('./routes/getImageRoute');
 
+const app = express();
+
+// CORS options to allow all origins
 const corsOptions = {
-    origin: 'https://web3-vault-front.vercel.app/',  // Only allow this domain
+    origin: '*',  // Allow all origins
     methods: 'GET, POST, PUT, DELETE',  // Allowed methods
     allowedHeaders: 'Content-Type, Authorization',  // Allowed headers
     credentials: true  // Allow credentials (cookies, tokens, etc.)
-  }
+};
 
+app.use(cors(corsOptions));  // Apply CORS middleware globally
 
-// app.use(cors(corsOption))
-app.options('*', cors(corsOptions)); // Enable pre-flight requests for all routes
+app.use(express.json());
 
-app.use(express.json())
+app.use('/api', authenticationRoute);
+app.use('/api', uploadImageRoute);
+app.use('/api', getImageRoute);
 
-app.use('/api',authenticationRoute)
-
-app.use('/api',uploadImageRoute)
-app.use('/api',getImageRoute)
-
-app.get('/', (req ,res)=> {
+app.get('/', (req, res) => {
     res.send('<h1> hi </h1>');
-})
+});
 
-
-async function serverStart(){
-
+async function serverStart() {
     try {
-        await connectDB(MONGODB_URL)
-        console.log("Connected to database")
-        app.listen(PORT,()=>{
-            console.log("server is running")
-        }) 
+        await connectDB(MONGODB_URL);
+        console.log("Connected to database");
+        app.listen(PORT, () => {
+            console.log("Server is running");
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
-serverStart()
+serverStart();
